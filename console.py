72442fb788291e0f69c59f0f365ff4ac6e6c602b
Rebecca Bell <rebeccaromanobell@gmail.com>
2023-02-03 23:37:31 +0000

    added styling to trips page

--- console.py
+++ console.py
@@ -8,19 +8,35 @@ import repositories.city_repository as city_repository
 import repositories.trip_repository as trip_repository
 
 country1 = Country("Italy")
-country_repository.save(country1)
 country2 = Country("Spain")
+country3 = Country("Austria")
+country4 = Country("Portugal")
+country5 = Country("France")
+country_repository.save(country1)
 country_repository.save(country2)
+country_repository.save(country3)
+country_repository.save(country4)
+country_repository.save(country5)
 
 city1 = City("Rome", country1.id)
-city_repository.save(city1)
 city2 = City("Madrid", country2.id)
+city3 = City("Vienna", country3.id)
+city4 = City("Lisbon", country4.id)
+city5 = City("Paris", country5.id)
+city_repository.save(city1)
 city_repository.save(city2)
+city_repository.save(city3)
+city_repository.save(city4)
+city_repository.save(city5)
 
 trip1 = Trip(city1.id, city1.country_id, "Food, Culture", "Spring", "In the next year")
-trip_repository.save(trip1)
-
-trip2 = Trip(city2.id, city2.country_id, "Architecture, History", "Autumn", "In the next 2 years"
+trip2 = Trip(
+    city2.id, city2.country_id, "Architecture, History", "Autumn", "In the next 2 years"
 )
+trip3 = Trip(
+    city3.id, city3.country_id, "History, Scenery", "Winter", "In the next year"
+)
+trip_repository.save(trip1)
 trip_repository.save(trip2)
+trip_repository.save(trip3)
 pdb.set_trace()
